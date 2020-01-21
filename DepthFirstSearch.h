069eafc0e0221da2bbc066fb3a68c535ef00e6f5
dorefaeli <dorefaeli@gmail.com>
2020-01-21 18:05:55 +0200

    Added DepthFirstSearch class

---
+++ DepthFirstSearch.h
@@ -0,0 +1,18 @@
+//
+// Created by Dor Refaeli on 21/01/2020.
+//
+
+#ifndef ALGORITHMICPROGRAMMING2_DEPTHFIRSTSEARCH_H
+#define ALGORITHMICPROGRAMMING2_DEPTHFIRSTSEARCH_H
+
+#include "Searcher.h"
+#include "Searchable.h"
+#include "State.h"
+
+template <class P, class S>
+class DepthFirstSearch {
+public:
+    S search(Searchable<P> problem) override;
+};
+
+#endif //ALGORITHMICPROGRAMMING2_DEPTHFIRSTSEARCH_H
